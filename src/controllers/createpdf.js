81ec70bdc218e52824ea9a52a992fc0a7c183a1c
fahmiazis <fahmiazis797@gmail.com>
2021-11-12 08:33:57 +0700

    update

--- src/controllers/createpdf.js
+++ src/controllers/createpdf.js
@@ -3,12 +3,12 @@ const response = require('../helpers/response')
 module.exports = {
   createPdf: async (req, res) => {
     try {
-      const { depo, promo, tgldari, timeStart } = req.query
+      const { depo, promo, tgldari, reg } = req.query
       const Api2Pdf = require('api2pdf')
       const a2pClient = new Api2Pdf('9e9db4c1-997d-4a14-a3e1-d830034da05c')
 
       const options = { orientation: 'landscape', pageSize: 'A4', title: '0034/SLS-FHO/I/21A.pdf' }
-      const url = `http://trial.pinusmerahabadi.co.id/redpinereport/library/export/byDepo/export/export_promo_tpr_automaticaly_pdf.php?depo=${depo}&promo=${promo}&tgldari=${tgldari}&timeStart=${timeStart}`
+      const url = `http://trial.pinusmerahabadi.co.id/redpinereport/library/export/byDepo/export/export_promo_tpr_automaticaly_pdf.php?depo=${depo}&promo=${promo}&tgldari=${tgldari}&reg=${reg}`
       await a2pClient.wkUrlToPdf(url, { options: options }).then(function (result) {
         return response(res, 'succes create pdf', { result, url })
       })
